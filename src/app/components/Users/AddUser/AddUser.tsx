39beec0b1cb20e4dea6c47c342215088a6660dc8
fatmakhv <fatmaba@gmail.com>
2023-09-02 16:24:18 +0300

    Completed

--- src/app/components/Users/AddUser/AddUser.tsx
+++ src/app/components/Users/AddUser/AddUser.tsx
@@ -1,22 +1,52 @@
 'use client';
 
-import React, { FC, FormEvent } from 'react';
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import Button from '../../UI/Button/Button';
 import Card from '../../UI/Card/Card';
+
 import './AddUser.css';
 
 const AddUser: FC = (): JSX.Element => {
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
+
     const addUserHandler: any = (e: FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
+        if ( username.trim().length === 0 || age.trim().length === 0 || +age < 1) {
+            return;
+        }
+        setUsername('');
+        setAge('');
     }
+
+    const usernameChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+        e.preventDefault();
+        setUsername(e.target.value);
+    };
+
+    const ageChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+        e.preventDefault();
+        setAge(e.target.value);
+    };
 
     return (
         <Card cssName="input">
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"></input>
+                <input
+                    id="username"
+                    type="text"
+                    value={username}
+                    onChange={usernameChangeHandler}>
+                </input>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="text"></input>
-                <button type="submit">Add User</button>
+                <input
+                    id="age"
+                    type="text"
+                    value={age}
+                    onChange={ageChangeHandler}>
+                </input>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     )
